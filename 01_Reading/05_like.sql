8790cb87456b7b04c491e2abcc9ff922cce01f58
Daniel Huarhua <167722543+DanielHuarhua@users.noreply.github.com>
2025-06-16 10:18:17 -0500

    05_like.sql

--- 01_Reading/05_like.sql
+++ 01_Reading/05_like.sql
@@ -1,6 +1,9 @@
 /*
 LIKE
-Lección 9.4: https://youtu.be/OuJerKzV5T0?t=6894
+Se utiliza para buscar un patron especifico en una columa. Se usa los comodines:
+%: Representa cero, uno o varios caracteres comdojnes
+-: Representa un solo caracter comodin
+SELECT carac1,carac2,... FROM tabla WHERE caracteristica LIKE 'patron'
 */
 
 -- Obtiene todos datos de la tabla "users" que contienen un email con el texto "gmail.com" en su parte final
@@ -9,5 +12,5 @@ SELECT * FROM users WHERE email LIKE '%gmail.com';
 -- Obtiene todos datos de la tabla "users" que contienen un email con el texto "sara" en su parte inicial
 SELECT * FROM users WHERE email LIKE 'sara%';
 
--- Obtiene todos datos de la tabla "users" que contienen un email una arroba
+-- Obtiene todos datos de la tabla "users" que contienen una @ en su email
 SELECT * FROM users WHERE email LIKE '%@%';
