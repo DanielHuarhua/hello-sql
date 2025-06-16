5e49b22305ecec7096a89757e22c100c76b99a55
Daniel Huarhua <167722543+DanielHuarhua@users.noreply.github.com>
2025-06-16 15:18:18 -0500

    18_having.sql

--- 01_Reading/18_having.sql
+++ 01_Reading/18_having.sql
@@ -1,7 +1,11 @@
 /*
 HAVING
-Lección 10.12: https://youtu.be/OuJerKzV5T0?t=9265
+Filtra los resultados luego de agruparlos.
+A diferencia de where, WHERE filtra las filas antes de la agregacion
+HAVING filtra cada grupo resultante luego de aplicar su funcion de agregacion
 */
 
 -- Cuenta cuantas filas contienen un dato no nulo en el campo edad de la tabla "users" mayor que 3
-SELECT COUNT(age) FROM users HAVING COUNT(age) > 3
+SELECT age,COUNT(age) FROM users GROUP BY age HAVING COUNT(age) > 3
+-- Agrupumos las filas por la edad, y contamos cuantos elementos tiene cada grupo
+-- Mostramos la edad y el numero de elementos del grupo solo si el grupo tiene mas de 3 elementos
