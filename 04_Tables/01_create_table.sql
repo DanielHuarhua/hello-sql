64571bffec0c1c94b9f52ddeffb0f1b0a40380f5
Daniel Huarhua <167722543+DanielHuarhua@users.noreply.github.com>
2025-07-16 10:52:31 -0500

    01_create_table.sql

--- 04_Tables/01_create_table.sql
+++ 04_Tables/01_create_table.sql
@@ -5,25 +5,24 @@ Se utiliza para crear una nueva tabla en una base de datos
 
 -- Crea una tabla llamada "persons" con nombre de columna (atributos) de tipo int, varchar y date
 CREATE TABLE persons (
-    id int,
-    name varchar(100), 
-    age int,
+    id int, -- Se debe especificar el tipo de dato
+    name varchar(100), -- Se debe especificar la cantidad de Caracteres
+    age int, -- Cada atributo se separa por una coma 
     email varchar(50),
-    created date
+    created date -- El ultimo atributo no se agrega coma
 );
 
 /*
 CONSTRAINTS: Restricciones
+Permiten poner condiciones a los atributos
 */
 
 /*
 NOT NULL
-Lección 13.2: https://youtu.be/OuJerKzV5T0?t=11619
+Obliga a que el campo id posea siempre un valor no nulo
 */
-
--- NOT NULL: Obliga a que el campo id posea siempre un valor no nulo
 CREATE TABLE persons2 (
-	id int NOT NULL,
+    id int NOT NULL,
     name varchar(100) NOT NULL,
     age int,
     email varchar(50),
@@ -32,43 +31,38 @@ CREATE TABLE persons2 (
 
 /*
 UNIQUE
-Lección 13.3: https://youtu.be/OuJerKzV5T0?t=11787
+Garantiza que los valores en una columna sean unicos
 */
-
--- UNIQUE: Obliga a que el campo id posea valores diferentes
 CREATE TABLE persons3 (
-	id int NOT NULL,
+    id int NOT NULL,
     name varchar(100) NOT NULL,
     age int,
     email varchar(50),
     created datetime,
-    UNIQUE(id)
+    UNIQUE(id), -- Si deseo añadir mas columnas con registros unicos, debo definirlo uno por uno
+    UNIQUE (name)
 );
 
 /*
 PRIMARY KEY
-Lección 13.4: https://youtu.be/OuJerKzV5T0?t=11911
+Sirve como identificador para futuras relaciones. Será Unico y No Nulo
 */
-
--- PRIMARY KEY: Establece el campo id como clave primaria para futuras relaciones con otras tablas
 CREATE TABLE persons4 (
-	id int NOT NULL,
+    id int NOT NULL,
     name varchar(100) NOT NULL,
     age int,
     email varchar(50),
     created datetime,
     UNIQUE(id),
-    PRIMARY KEY(id)
+    PRIMARY KEY(id,name) -- Si quiero definir varias columnas como PRIMARY KEY, puedo añadirlas dentro del mismo argumento
 );
 
 /*
 CHECK
-Lección 13.5: https://youtu.be/OuJerKzV5T0?t=12121
+Garantiza que todos los valores en una columna cumplan con una condicion especifica
 */
-
--- CHECK: Establece que el campo age sólo podrá contener valores mayores o iguales a 18
 CREATE TABLE persons5 (
-	id int NOT NULL,
+    id int NOT NULL,
     name varchar(100) NOT NULL,
     age int,
     email varchar(50),
@@ -80,16 +74,14 @@ CREATE TABLE persons5 (
 
 /*
 DEFAULT
-Lección 13.6: https://youtu.be/OuJerKzV5T0?t=12243
+Establece un valor por defecto a una columna cuando no se especifica durante la insercion de datos.
 */
-
--- DEFAULT: Establece un valor por defecto en el campo created correspondiente a la fecha del sistema
 CREATE TABLE persons6 (
-	id int NOT NULL,
+    id int NOT NULL,
     name varchar(100) NOT NULL,
     age int,
     email varchar(50),
-    created datetime DEFAULT CURRENT_TIMESTAMP(),
+    created datetime DEFAULT (CURRENT_DATE), -- CURRENT_DATE: Retorna la fecha actual
     UNIQUE(id),
     PRIMARY KEY(id),
     CHECK(age>=18)
@@ -97,12 +89,10 @@ CREATE TABLE persons6 (
 
 /*
 AUTO INCREMENT
-Lección 13.7: https://youtu.be/OuJerKzV5T0?t=12362
+Incrementar automaticamente el valor de una columna si esta no ha sido especificada
 */
-
--- AUTO_INCREMENT: Indica que el campo id siempre se va a incrementar en 1 con cada nuevo inserto
 CREATE TABLE persons7 (
-	id int NOT NULL AUTO_INCREMENT,
+    id int NOT NULL AUTO_INCREMENT,
     name varchar(100) NOT NULL,
     age int,
     email varchar(50),
