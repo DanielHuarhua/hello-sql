1723534f864aec41e578a0f4b9346c43cbc3c793
Daniel Huarhua <167722543+DanielHuarhua@users.noreply.github.com>
2025-07-29 22:21:12 -0500

    01_inner_join.sql

--- 05_Join/01_inner_join.sql
+++ 05_Join/01_inner_join.sql
@@ -1,51 +1,38 @@
 /*
 INNER JOIN (JOIN)
-Lección 17.1: https://youtu.be/OuJerKzV5T0?t=16101
+Combina filas de dos tablas solo cuando hay una coincidencia em ambas.
+Un usuario sin DNI y yn DNI que no este asociado a un usuario no apareran en la consulta,
+solo aquellos que coinciden en ambas tablas
+
+SELECT columna1,columna2
+FROM table1
+INNER JOIN table2
+ON table1.column_name = table2.column_name;
 */
-
--- Realiza un JOIN de manera incorrecta, ya que no existe un campo de relación
-SELECT * FROM users
-INNER JOIN dni;
-
--- Obtiene los datos de los usuarios que tienen un dni 
+-------------------------------------------------------------------------------------------------------
+1:1
+-- Obtiene todos los datos de ambas tablas que tienen coincidencia en el user_id (JOIN ES LO MISMO QUE INNER JOIN)
 SELECT * FROM users
 INNER JOIN dni
 ON users.user_id = dni.user_id;
 
--- Obtiene los datos de los usuarios que tienen un dni (JOIN es lo mismo que INNER JOIN)
-SELECT * FROM users
-JOIN dni
-ON users.user_id = dni.user_id;
-
--- Obtiene el nombre y el dni de los usuarios que tienen un dni y los ordena por edad
+-- Obtiene el nombre de la tabla1 y el dni de la tabla2 de los usuarios que tienen coincidencia
 SELECT name, dni_number FROM users
 JOIN dni
 ON users.user_id = dni.user_id
 ORDER BY age ASC;
-
--- Obtiene los datos de los usuarios que tienen empresa
-SELECT * FROM users
-JOIN companies
-ON users.company_id = companies.company_id;
-
--- Obtiene los datos de las empresas que tienen usuarios
-SELECT * FROM companies
-JOIN users
-ON users.company_id = companies.company_id;
-
--- Obtiene el nombre de las empresas junto al nombre de sus usuarios
-SELECT companies.name, users.name FROM companies
-JOIN users
-ON companies.company_id = users.company_id;
-
--- Obtiene los nombres de usuarios junto a los lenguajes que conocen
-SELECT users.name, languages.name
-FROM users_languages
-JOIN users ON users_languages.user_id=users.user_id
-JOIN languages ON users_languages.language_id=languages.language_id;
-
--- Obtiene los nombres de usuarios junto a los lenguajes que conocen (utilizando otro orden de relación entre tablas)
-SELECT users.name, languages.name
-FROM users
-JOIN users_languages ON users.user_id=users_languages.user_id
-JOIN languages ON users_languages.language_id=languages.language_id;
+---------------------------------------------------------------------------------------------------------------
+1:N
+--Obtiene el departamento y el nombre de los trabajadores en dicho departamento, un trabajador puede estar
+en vaios departamentos, pero en cada departamento solo hay un trabajador
+SELECT nombre,name,age FROM departamentos
+INNER JOIN users
+ON departamentos.user_id=users.user_id;
+--------------------------------------------------------------------------------------------------------------------
+N:M
+-- Obtiene los nombres de usuarios junto al area al que trabajan
+-- Un usuario puede trabajar en muchas area y en un area puede trabajar varios usuarios
+SELECT trabajadores.name, area.name
+FROM trabajadores_area
+JOIN trabajadores ON trabajadores_area.trabajador_id=trabajadores.trabajador_id
+JOIN area ON trabajadores_area.area_id=area.area_id;
