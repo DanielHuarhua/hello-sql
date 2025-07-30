129af64cb492cc5d5d0f120a06949b88356b43ac
Daniel Huarhua <167722543+DanielHuarhua@users.noreply.github.com>
2025-07-29 23:20:30 -0500

    02_left_join.sql

--- 05_Join/02_left_join.sql
+++ 05_Join/02_left_join.sql
@@ -1,22 +1,14 @@
 /*
 LEFT JOIN
-Lección 17.2: https://youtu.be/OuJerKzV5T0?t=17045
+Combina filas de dos tablas, pero incluye todas las filas de la tabla izquierda (tabla1),
+tengan o no coincidencias. Si no hay coincidencia en la tabla derecha (tabla2) aparecera como nulo
 */
 
--- Obtiene los datos de todos los usuarios junto a su dni (lo tenga o no)
-SELECT * FROM users
-LEFT JOIN dni
-ON users.user_id = dni.user_id;
-
--- Obtiene el nombre de todos los usuarios junto a su dni (lo tenga o no)
-SELECT name, dni_number FROM users
-LEFT JOIN dni
-ON users.user_id = dni.user_id;
-
--- Obtiene todos los dni junto al nombre de su usuario (lo tenga o no)
-SELECT name, dni_number FROM dni
-LEFT JOIN users
-ON users.user_id = dni.user_id;
+-- Obtiene el nombre de todos los usuarios junto a su dni (tengan un DNI asociado o no)
+SELECT name, dni_number FROM users (tabla izquierda)
+LEFT JOIN dni (tabla derecha)
+ON users.user_id = dni.user_id
+ORDER BY name ASC;
 
 -- Obtiene el nombre de todos los usuarios junto a sus lenguajes (los tenga o no)
 SELECT users.name, languages.name
