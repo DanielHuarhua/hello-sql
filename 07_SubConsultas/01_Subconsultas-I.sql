133f5b3960871c7d98b10531d80ab0901716aa24
Daniel Huarhua <167722543+DanielHuarhua@users.noreply.github.com>
2025-08-17 10:25:50 -0500

    01_Subconsultas-I.sql

--- 07_SubConsultas/01_Subconsultas-I.sql
+++ 07_SubConsultas/01_Subconsultas-I.sql
@@ -1,1 +1,5 @@
-
+SUBCONSULTA
+-- También llamada consulta anidada es una consulta SQL que se escribe de otra consulta principal:
+* Se coloca entre parentesis    
+* La subconsulta devuelve un valor o conjunto de valores que la consulta princpial puede usar  
+* Ayuda a resolver problemas que no se pueden obtener fácilmente con una sola consulta.
