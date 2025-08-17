9ffa2fd8f4638ef8a794be3106ae34919fb35348
Daniel Huarhua <167722543+DanielHuarhua@users.noreply.github.com>
2025-08-17 10:59:59 -0500

    02_Tipo_de_Sub-Consultas.sql

--- 07_SubConsultas/02_Tipo_de_Sub-Consultas.sql
+++ 07_SubConsultas/02_Tipo_de_Sub-Consultas.sql
@@ -1,1 +1,16 @@
-
+/*
+1. SUB-CONSULTA ESCALAR
+La subconsulta devuelve un unico valor, (generalemente una funcion de agregacion), que la consulta
+principal utilizará
+*/
+SELECT * FROM users WHERE age > (SELECT avg(age) FROM USERS);
+-- La subconsulta retorna un unico valor (media), que será usado en la consulta principal
+--------------------------------------------------------------------------------------------------------
+/*
+2. SUB-CONSULTA DE COLUMNA ÚNICA
+La subconsulta retorna una columna con varias filas, donde suelen usarse operadores como IN,ALL,ANY
+*/
+SELECT user_id,dni_number FROM dni WHERE user_id IN (
+SELECT user_id FROM users WHERE age>14);
+-- La subconsulta retorna varias filas de otra tabla que cumplen una condicion, esto resutados seran
+usados en la consulta principal usando otra tabla.  
